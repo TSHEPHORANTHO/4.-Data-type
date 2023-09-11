fddc2b6162a296b4cbcde88c6b76972880d0037d
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-11 10:57:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring  of string, interger, a boolean and decimal
+            string name = "Budha";
+            int age = 50;
+            bool isTall = true;
+            double weight = 80.2;
+            char initial = 'B';
+
+            //print 
+            Console.WriteLine("His name is " + name + " he is " + age + " it is " + isTall + " that he is tall");
+            Console.WriteLine("His weight in kg's is " + weight + " his initial is " + initial);
+           
+            //freeze
+            Console.ReadLine();
+        }
+    }
+}
